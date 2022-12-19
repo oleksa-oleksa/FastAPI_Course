53cb33af0c3e5bf38d3690a67c7047360bf5fa8f
Baga <obaga@deloitte.de>
2022-12-19 16:42:48 +0100

    get all books

---
+++ books.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = {
+    "book_1": {"title": "Title One", "author": "Author One"},
+    "book_2": {"title": "Title Two", "author": "Author Two"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"}
+}
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/books/mybook")
+async def read_favorite_book():
+    return {"book_title": "My favorite book"}
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    return {"book_title": book_id}
