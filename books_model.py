17d4950038a033e7ca63b99e46c1e0e2990b288d
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-12-20 11:12:20 +0100

    put() for book updating

--- books_model.py
+++ books_model.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 class Book(BaseModel):
     id: UUID
-    title: str = Field(min_length=1)
+    title: str = Field(min_length=1, max_length=100)
     author: str = Field(min_length=1, max_length=50)
     description: Optional[str] = Field(title="Description of the book",
                              min_length=1, 
@@ -59,28 +59,33 @@ async def create_book(book: Book):
 @app.put("/{book_id}")
 async def update_book(book_id: UUID, book: Book):
     counter = 0
-    
+    for b in BOOKS:
+        if b.id == book_id:
+            BOOKS[counter] = book
+            return BOOKS[counter]
+        counter += 1
+
 
 def create_book_no_api():
-    book_1 = Book(id="3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    book_1 = Book(id="fe8e60f1-b870-45b7-ac8a-35fc68ec2b92",
                   title = "Title_1",
                   author="Author_1",
                   description="Description_1",
                   rating=13)
 
-    book_2 = Book(id="3fa85f64-5717-4562-b3fc-2c963f66e456",
+    book_2 = Book(id="785ad051-17ed-48dd-927f-b4ab95cd1548",
                   title = "Title_2",
                   author="Author_2",
                   description="Description_2",
                   rating=75)   
     
-    book_3 = Book(id="3fa85f64-5717-4562-b3fc-2c963f66e123",
+    book_3 = Book(id="4649b7ec-5b81-4f70-a41b-30c7d60fe669",
                   title = "Title_3",
                   author="Author_3",
                   description="Description_3",
                   rating=60)   
 
-    book_4 = Book(id="3fa85f64-5717-4562-b3fc-3e963f66e456",
+    book_4 = Book(id="f3c6fc14-4bca-412c-8bda-7f6c90f2565b",
                   title = "Title_4",
                   author="Author_4",
                   description="Description_4",
