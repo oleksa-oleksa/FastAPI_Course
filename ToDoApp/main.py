efb0ed6511102c5c9d015be0ecd9e8e88b361db7
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-12-28 17:25:19 +0100

    deleted todo item from the database

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -28,7 +28,7 @@ async def read_all(db : Session = Depends(get_db)):
 
 
 @app.get("/todo/{todo_id}")
-async def read_todo(todo_id : int, db : Session = Depends(get_db)):
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
 
     if todo_model is not None:
@@ -37,17 +37,51 @@ async def read_todo(todo_id : int, db : Session = Depends(get_db)):
 
 
 @app.post("/")
-async def create_todo(todo : Todo, db = Session = Depends(get_db)):
+async def create_todo(todo: Todo, db = Session = Depends(get_db)):
     todo_model = models.Todos()
     todo_model.title = todo.title
     todo_model.description = todo.description
+    todo_model.priority = todo.priority
     todo_model.complete = todo.complete
 
     db.add(todo_model)
     db.commit()
 
-    return {"status" : 201,
-            "transaction": "successful"}
+    successful_response(201)
+
+@app.put("/")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_exception()
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add()
+    db.commit()
+
+    successful_response(201)
+
+@app.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_exception()
+
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+
+    db.commit()
+
+    successful_response(201)
 
 def http_exception():
     raise HTTPException(status_code=404, detail="Todo ID not found!")
+
+def successful_response(status_code: int):
+    return {"status" : status_code,
+            "transaction": "successful"}
