8dc6bc26fd748383dccc14babfa0484fb655134f
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-12-28 17:08:26 +0100

    post new todo item into database

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, Base, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -14,10 +16,38 @@ def get_db():
     finally:
         db.close()
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, ls=3, description="Priority must be between 0 and 3")
+    complete: bool
+
 @app.get("/")
 async def read_all(db : Session = Depends(get_db)):
     return db.query(models.Todos).all()
 
-@app.get("/")
-async def create_database():
-    return {"Database": "Created"}
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id : int, db : Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+    raise http_exception()
+
+
+@app.post("/")
+async def create_todo(todo : Todo, db = Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {"status" : 201,
+            "transaction": "successful"}
+
+def http_exception():
+    raise HTTPException(status_code=404, detail="Todo ID not found!")
