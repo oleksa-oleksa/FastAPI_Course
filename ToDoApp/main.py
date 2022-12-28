4a7552313ac7fa7f3159783ad273039f59c28d65
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-12-28 16:42:50 +0100

    Resume after Christmas brake

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine, Base
+from database import engine, Base, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+async def read_all(db : Session = Depends(get_db)):
+    return db.query(models.Todos).all()
 
 @app.get("/")
 async def create_database():
