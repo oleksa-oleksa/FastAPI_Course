5a23300d1ae98ea874eed762410078c72b21e93e
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-12-29 16:19:19 +0100

    hashed bcrypt password

--- ToDoApp/auth.py
+++ ToDoApp/auth.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel 
 from typing import Optional
 import models
+from passlib.context import CryptContext
 
 class CreateUser(BaseModel):
     username: str
@@ -10,8 +11,12 @@ class CreateUser(BaseModel):
     last_name: str
     password: str
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
+
+def get_password_hash(password):
+    return bcrypt_context.hash(password)
 
 @app.post("/create/user")
 async def create_new_user(new_user: CreateUser):
@@ -19,7 +24,7 @@ async def create_new_user(new_user: CreateUser):
     create_user_model.email = new_user.email
     create_user_model.first_name = new_user.first_name
     create_user_model.last_name = new_user.last_name
-    create_user_model.hashed_password = new_user.password
+    create_user_model.hashed_password = get_password_hash(new_user.password)
     create_user_model.is_active = True
 
     return create_user_model
