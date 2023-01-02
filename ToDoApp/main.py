f1f85f9250167e30cdd1ad27d97a1427956b767b
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-01-02 12:48:06 +0100

    starting with Postman

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -4,6 +4,7 @@ from database import engine, Base, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from typing import Optional
+import auth as auth
 
 app = FastAPI()
 
@@ -25,6 +26,14 @@ class Todo(BaseModel):
 @app.get("/")
 async def read_all(db : Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todos/user")
+async def read_all_by_user(user: dict = Depends(auth.get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise auth.get_user_exception()
+    return db.query(models.Todos).filter(models.Todos.owner_id == user.get("id")).all()
+
 
 
 @app.get("/todo/{todo_id}")
