2e37185123361505d1ee658877ce8827403728b7
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-01-02 14:47:47 +0100

    connect PostgreSQL, MySQL

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -4,11 +4,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://superuser:mypass@localhost/ToDoAppDatabase"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://superuser:mypass@127.0.0.1:3306/todoappdb"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 
 )
+
+# PostgreSQL, MySQL
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
